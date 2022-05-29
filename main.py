e0767099ec40bf770e53fd282c2ff5791e4a2bae
Sofiia Levchenko <sofia.levchenko@gmail.com>
2022-05-29 21:49:26 +0200

    feat: m/1/1 first version

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from simulator import Simulator
 
 def main():
     simulator = Simulator()
-    simulator.start_mm1(1000, 4, 2)
+    simulator.start_mm1(100, 7, 8)
 
 if __name__ == "__main__":
     main()
--- simulator.py
+++ simulator.py
@@ -1,4 +1,3 @@
-from packet import Packet
 import numpy as np
 
 class Simulator:
@@ -15,67 +14,67 @@ class Simulator:
         self.action_type = None
         self.queue = []
 
-    def generate_packet_mm1(self, bit_rate):        
-        return np.random.exponential(1. / 3), np.random.exponential(1. / 4)
+    def get_time_between_arrivals(self, num):
+        return np.random.exponential(1/num)
+
+    def get_time_of_service(self, num):
+        return np.random.exponential(1/num)
     
-    def init_action_lists(self, bit_rate):
-        packet_arival, _ = self.generate_packet_mm1(bit_rate)
+    def init_action_lists(self, lambda_val):
+        packet_arival = self.get_time_between_arrivals(lambda_val)
         self.packet_arrival = self.time + packet_arival
 
-    def action_a_algorithm(self, bit_rate, processing_rate):
-        packet_arival, packet_length = self.generate_packet_mm1(bit_rate)
+    def action_a_algorithm(self, lambda_val, mi_val):
+        packet_arival = self.get_time_between_arrivals(lambda_val)
         self.packet_arrival = self.time + packet_arival
         if self.server_status == 0:
-            self.num_of_delay = self.num_of_delay + 1
             self.server_status = 1
-            self.packet_process_end = self.time + packet_length
+            self.num_of_delay += 1
+            time_of_service = self.get_time_of_service(mi_val)
+            self.packet_process_end = self.time + time_of_service
         else:
+            self.time_wait_in_queue += len(self.queue)*(self.time - self.prev_time)
             self.queue.append(self.time)
-        self.time_busy_server = self.time_busy_server + (self.time - self.prev_time)*self.server_status
-        self.time_wait_in_queue = self.time_wait_in_queue + len(self.queue)*(self.time - self.prev_time)
+            self.time_busy_server += (self.time - self.prev_time)*self.server_status
     
-    def action_d_algorithm(self, bit_rate, processing_rate):
+    def action_d_algorithm(self, mi_val):
         if len(self.queue) == 0:
             self.server_status = 0
             self.packet_process_end = -1
+            self.time_busy_server += (self.time - self.prev_time)*self.server_status
         else:
-            self.num_of_delay = self.num_of_delay + 1
+            self.num_of_delay += 1
             self.delay = self.delay + (self.time - self.queue[0])
-            self.time_busy_server = self.time_busy_server + (self.time - self.prev_time)*self.server_status
-            self.time_wait_in_queue = self.time_wait_in_queue + len(self.queue)*(self.time - self.prev_time)
-            _, packet_length = self.generate_packet_mm1(bit_rate)
-            self.packet_process_end = self.time + packet_length
+            self.time_wait_in_queue += len(self.queue)*(self.time - self.prev_time)
+            time_of_service = self.get_time_of_service(mi_val)
+            self.packet_process_end = self.time + time_of_service
             self.queue.pop(0)
+            self.time_busy_server += (self.time - self.prev_time)*self.server_status
     
     def update_clock(self):
         self.prev_time = self.time
         if (self.packet_arrival < self.packet_process_end) or (self.packet_process_end == -1):
             self.time = self.packet_arrival
             self.action_type = "A"
-        else:
+        elif (self.packet_arrival > self.packet_process_end):
             self.time = self.packet_process_end
             self.action_type = "D"
 
-    def start_mm1(self, simulation_time, bit_rate, processing_rate):
-        self.init_action_lists(bit_rate)
-        while(self.num_of_delay < simulation_time):
+    def start_mm1(self, simulation_time, lambda_val, mi_val):
+        self.init_action_lists(lambda_val)
+        while(self.time < simulation_time):
             self.update_clock()
             if (self.action_type == "A"):
-                self.action_a_algorithm(bit_rate, processing_rate)
+                self.action_a_algorithm(lambda_val, mi_val)
             elif (self.action_type == "D"):
-                self.action_d_algorithm(bit_rate, processing_rate)
+                self.action_d_algorithm(mi_val)
         print("number of packets: ", self.num_of_delay)
         print("mean delay for client: ", self.delay/self.num_of_delay)
-        print("mean time in queue: ", self.time_wait_in_queue/self.num_of_delay)
+        print("mean clients in queue: ", self.time_wait_in_queue/self.time)
         print("server busy: ", self.time_busy_server/self.time)
-        # print( self.delay, self.time_busy_server, self.time_wait_in_queue, )
 
     def start_fifo(self, packet_length, bit_rate, num_of_sources):
         pass
         
     def start_prio(self):
         pass
-
-#  speed: 300b/s 
-#  deltaT: 5s
-#  1500b -> 1 pakiet
